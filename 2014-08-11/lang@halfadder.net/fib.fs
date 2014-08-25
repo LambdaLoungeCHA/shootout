f8734bbac5822b995abc0a196d26ed1e03624b27
Lang Martin <lmartin@opentable.com>
2014-08-25 10:07:03 -0400

    forth fib

---
+++ 2014-08-11/lang@halfadder.net/fib.fs
@@ -0,0 +1,24 @@
+: fib ( n -- fibn )
+    dup 1 > if
+	1- dup 1-			\ -1 -2
+	recurse				\ -1 fib(-2)
+	swap
+	recurse				\ fib(-2) fib(-1)
+	+
+    then
+;
+
+\ 9 fib dec.
+
+: rabbit ( kit gen -- kit n )
+    dup 1 > if
+	over swap			\ k k n
+	1- 2dup				\ k k n-1 k n-1
+	recurse *			\ k k n-1 k*fn-1
+	-rot
+	1- recurse *			\ k k*fn-1 k*fn-2
+	+
+    then
+;
+
+\ 3 5 rabbit dec. drop
