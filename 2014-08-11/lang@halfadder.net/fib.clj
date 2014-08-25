bcfa2b582ed8a105f238e3655c0e7ddaf4f0d8ca
Lang Martin <lmartin@opentable.com>
2014-08-25 10:58:16 -0400

    clojure fib: fib has two defined numbers

--- 2014-08-11/lang@halfadder.net/fib.clj
+++ 2014-08-11/lang@halfadder.net/fib.clj
@@ -1,15 +1,23 @@
 (ns fib
   (:use clojure.test))
 
-(def fib-naive
-  (fn fib
-    ([n] (fib n 1))
-    ([n k]
-       (if (< n 2)
-         n
-         (+ (fib (- n 1))
-            (fib (- n 2)))))))
+(defn fib-naive
+  ([n] (fib-naive n 1))
+  ([n k]
+     (cond (< n 1) 0
+           (< n 2) 1
+           :else
+           (+ (fib-naive (- n 1) k)
+              (* (fib-naive (- n 2) k)
+                 k)))))
 
 (deftest test-fib
-  (is (= (fib-naive 5 3) 19))
-  (is (= (fib-naive 9))))
+  (is (= (for [x (range 1 10)]
+           (fib-naive x))
+         '(1 1 2 3 5 8 13 21 34)))
+
+  (is (= (for [x (range 1 10)]
+           (fib-naive x 3))
+         '(1 1 4 7 19 40 97 217 508)))
+
+  (is (= (fib-naive 5 3) 19)))
