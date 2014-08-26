78019a80f54f64bb21ec683104189646e4f9ada9
Noel Weichbrodt <noel@weichbrodt.org>
2014-08-26 00:11:46 -0400

    removed object literal return type

--- 2014-08-11/noel@weichbrodt.org/nucleotide.js
+++ 2014-08-11/noel@weichbrodt.org/nucleotide.js
@@ -2,25 +2,23 @@ var nucleotide = {
 
 	length: function(dna) {
 		var dna_array = dna.split('');
-		return {
-			a: dna_array.filter(function(e) { return e == 'A'; }).length,
-			c: dna_array.filter(function(e) { return e == 'C'; }).length,
-			g: dna_array.filter(function(e) { return e == 'G'; }).length,
-			t: dna_array.filter(function(e) { return e == 'T'; }).length
-		}						
+		return [dna_array.filter(function(e) { return e == 'A'; }).length, 
+				dna_array.filter(function(e) { return e == 'C'; }).length,
+				dna_array.filter(function(e) { return e == 'G'; }).length,
+				dna_array.filter(function(e) { return e == 'T'; }).length];					
 	}, 
 
 	test1: function() {
 		var input = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC';
-		var expected = {a:20, c:12, g:17, t:21};
+		var expected = [20, 12, 17, 21];
 		var actual = this.length(input);
-		return (actual.a === expected.a && actual.c === expected.c && actual.g === expected.g && actual.t === expected.t);
+		return (actual[0] === expected[0] && actual[1] === expected[1] && actual[2] === expected[2] && actual[3] === expected[3]);
 	},
 
 	test2: function() {
 		var input = 'GGTATTTTAATTTATAGT';
-		var expected = {a:5, c:0, g:3, t:10};
+		var expected = [5, 0, 3, 10];
 		var actual = this.length(input);
-		return (actual.a === expected.a && actual.c === expected.c && actual.g === expected.g && actual.t === expected.t);
+		return (actual[0] === expected[0] && actual[1] === expected[1] && actual[2] === expected[2] && actual[3] === expected[3]);
 	}
 }
